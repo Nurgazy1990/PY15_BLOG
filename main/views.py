1067dcd0b5f250e84ce969584668d1c023c328b2
addirossi <addirossi@gmail.com>
2022-01-04 21:20:54 +0600

    Add auth

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-from main.models import Category, Post
+from main.models import Category, Post, Comment
 from main.permissions import IsAuthor
-from main.serializers import CategorySerializer, PostSerializer, PostListSerializer
+from main.serializers import CategorySerializer, PostSerializer, PostListSerializer, CommentSerializer
 
 
 class CategoriesListView(ListAPIView):
@@ -31,6 +32,14 @@ class PostViewSet(ModelViewSet):
             return [IsAuthor()]
         # просматривать могут все
         return []
+
+
+class CommentViewSet(CreateModelMixin,
+                     UpdateModelMixin,
+                     DestroyModelMixin,
+                     GenericViewSet):
+        queryset = Comment.objects.all()
+        serializer_class = CommentSerializer
 
 #TODO: список категорий
 #TODO: CRUD постов
